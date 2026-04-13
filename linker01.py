6bf0b025ed7e655bf2900efb276f5986b2b1769e
Seeker1289 <168652232+Seeker1289@users.noreply.github.com>
2026-04-13 23:16:41 +0530

    Update linker01.py

--- linker01.py
+++ linker01.py
@@ -4,13 +4,11 @@ playlist_url = input("Enter Spotify playlist URL: ")
 
 html = requests.get(playlist_url).text
 
-# Collect all track links found in HTML
 links = set()
 for part in html.split('"'):
     if "open.spotify.com/track/" in part:
         links.add(part)
 
-# Save links to file
 with open("links.txt", "a") as f:
     for link in links:
         f.write(link + "\n")
