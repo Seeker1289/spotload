bd165db430a64203040bedb0bdb9568baf5a49e8
Seeker1289 <168652232+Seeker1289@users.noreply.github.com>
2026-04-13 23:00:29 +0530

    Create example_bash.sh

---
+++ example_bash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for word in $(cat ytlinks.txt); do
+	if [[ $word == "song" ]]; then
+		path="$1/songs"
+	elif [[ $word == "video" ]]; then
+		path="$1/videos"
+	else
+		python3 yt-dlp -x $word -P $path
+	fi
+done
+
+for word in $(cat nfslinks.txt); do
+	python3 yt-dlp -x $word -P $1/songs/nf
+done
+
+for word in $(cat nfvlinks.txt); do
+	python3 yt-dlp -x $word -P $1/videos/nf
+done
